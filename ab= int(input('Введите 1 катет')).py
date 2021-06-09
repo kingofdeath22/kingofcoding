0637c4668f71cbc4dbd22f7bdefee074a262f96b
kingofdeath22 <gggcx@mail.ru>
2021-06-10 06:56:46 +0700

    new

---
+++ ab= int(input('Введите 1 катет')).py
@@ -0,0 +1,12 @@
+ab= int(input('Введите 1 катет'))
+bc= int(input('Введите 2 катет'))
+ac= int(input('Введите гипотенузу'))
+print('Угл А находиться между 2 катетами и напротив гипотенузы(ab- прилежащий катет, а bc- противоположный)')
+sinA= bc/ac
+cosA= ab/ac
+tgA= bc/ab
+ctgA= ab/bc
+print('sinA= ',sinA)
+print('cosA= ',cosA)
+print('tgA= ',tgA)
+print('ctgA= ',ctgA)
